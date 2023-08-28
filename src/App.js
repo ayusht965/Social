a5d15d284be691afe50a96900267c0015629d138
ayusht965 <ak7046243581@gmail.com>
2023-08-28 19:53:26 +0530

    chore: code refactoring and issue fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
 
 function App(){
-    return<>
-       <Profile/>
-    </> 
-
-    
+    const {user} = useContext(AuthContext);
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element= {user ? <Home/> : <Register/>}/>
+                <Route path="/login" element={user ? <Navigate to="/" /> : <Login/>}/>
+                <Route path="/register" element={user ? <Navigate to="/" /> : <Register/>}/>
+                <Route path="/profile/:username" element={<Profile/>}/>
+            </Routes>
+        </BrowserRouter>
+    ) 
 }
 export default App;
